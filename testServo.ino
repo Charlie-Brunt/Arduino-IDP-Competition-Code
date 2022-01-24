c8d3d1b3ffd7f7b60572520bc442e89c7653dff9
Charlie-Brunt <cwhbrunt@gmail.com>
2022-01-24 12:20:04 +0000

    servo sweep

--- testServo.ino
+++ testServo.ino
@@ -1,13 +1,31 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+*/
+
 #include <Servo.h>
-Servo myservo;
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
 void setup() {
-    myservo.attach(2);
-    myservo.write(90);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-    myservo.write(90);
-    delau(1000);
-    myservo.write(0);
-    delay(1000);
+  for (pos = 0; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+  for (pos = 120; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
 }

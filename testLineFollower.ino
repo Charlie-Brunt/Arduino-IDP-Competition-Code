97292a7c2ab86be0fd5e37fbb3079eeeb3a0b05e
Charlie-Brunt <cwhbrunt@gmail.com>
2022-01-26 10:09:02 +0000

    test line follower

--- testLineFollower.ino
+++ testLineFollower.ino
@@ -82,10 +82,49 @@ void loop {
     }
 }
 
-void forward()
+void forwards(int speed) {
+    motor1 -> setSpeed(speed);
+    motor2 -> setSpeed(speed);
+    motor1 -> run(FORWARD);
+    motor2 -> run(FORWARD);
+}
 
-void turnright()
+void backwards(int speed) {
+    motor1 -> setSpeed(speed);
+    motor2 -> setSpeed(speed);
+    motor1 -> run(BACKWARD);
+    motor2 -> run(BACKWARD);
+}
 
-void turnleft()
+void turn_right(int speed_high,int speed_low) {
+    motor1 -> setSpeed(speed_high);
+    motor2 -> setSpeed(speed_low);
+    motor1 -> run(FORWARD);
+    motor2 -> run(FORWARD);
+}
 
-void brake()
+void turn_left(int speed_high,int speed_low) {
+    motor1 -> setSpeed(speed_low);
+    motor2 -> setSpeed(speed_high);
+    motor1 -> run(FORWARD);
+    motor2 -> run(FORWARD);
+}
+
+void rotate_right(int speed) {
+    motor1 -> setSpeed(speed);
+    motor2 -> setSpeed(speed);
+    motor1 -> run(FORWARD);
+    motor2 -> run(BACKWARD);
+}
+
+void rotate_left(int speed) {
+    motor1 -> setSpeed(speed);
+    motor2 -> setSpeed(speed);
+    motor1 -> run(BACKWARD);
+    motor2 -> run(FORWARD);
+}
+
+void stop() {
+    motor1 -> run(RELEASE);
+    motor2 -> run(RELEASE)
+}

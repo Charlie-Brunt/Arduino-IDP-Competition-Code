ee3848e48a0897869a749a5d5f841ed201b9d677
Amanda-Ge <qg224@cam.ac.uk>
2022-02-01 10:21:48 +0000

    red blue

---
+++ red_blue.ino
@@ -0,0 +1,186 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+// Pin assigments
+#define leftIn A0  // left IR sensor
+#define rightIn A1  // right IR sensor
+#define distanceSensorPin A2
+#define echoPin 2  // ultrasonic sensor I
+#define triggerPin 3  // ultrasonic sensor II
+#define courseLEDpin 1
+#define fineLEDpin 0
+#define pushButtonPin 7
+#define motionLEDpin 13
+
+// Motor setup
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
+
+// Flags, state variables
+int junctionCounter = 0;
+bool IfRotate = false; 
+bool IsOffLine = false;
+bool Ifdeliver = false;
+
+//Parameters
+const float motorSpeed = 255; // Adjust motor speed here
+const int duration_90degree = 4000; 
+const int duration_delivery = 3000;
+
+
+void setup()
+{
+    AFMS.begin();
+    pinMode(motionLEDpin, OUTPUT);
+    pinMode(leftIn, INPUT);
+    pinMode(rightIn, INPUT);
+
+    Serial.begin(9600);
+    Serial.println("Ready!");
+    delay(3000);
+}
+
+
+/******************************** MAIN PROGRAM ********************************/
+void loop()
+{   //left sensor state
+    int LineSensor1;
+    //right sensor state
+    int LineSensor2;
+    //sets left LineSensor1 to high if on tape, else Low
+    if(analogRead(leftIn)>=850){
+        LineSensor1 = HIGH;
+    }
+    else {
+        LineSensor1 = LOW;
+    }
+    //sets right LineSensor2 to high if on tape, else Low
+    if(analogRead(rightIn)>=850){
+        LineSensor2 = HIGH;
+    }
+    else {
+        LineSensor2 = LOW;
+    }  
+    if (Ifdeliver == true) {
+        red_box()
+    }
+    else{
+        line_follow(LineSensor1,LineSensor2);
+    }        
+}
+
+/******************************** MOVEMENT FUNCTIONS ********************************/
+void forwards(int speed)
+{
+    motor1->setSpeed(speed);
+    motor2->setSpeed(speed);
+    motor1->run(FORWARD);
+    motor2->run(FORWARD);
+}
+
+void backwards(int speed)
+{
+    motor1->setSpeed(speed);
+    motor2->setSpeed(speed);
+    motor1->run(BACKWARD);
+    motor2->run(BACKWARD);
+}
+
+void turn_right_forwards(int speed_high, int speed_low)
+{
+    motor1->setSpeed(speed_high);
+    motor2->setSpeed(speed_low);
+    motor1->run(FORWARD);
+    motor2->run(FORWARD);
+}
+
+void turn_left_forwards(int speed_high, int speed_low)
+{
+    motor1->setSpeed(speed_low);
+    motor2->setSpeed(speed_high);
+    motor1->run(FORWARD);
+    motor2->run(FORWARD);
+}
+
+void rotate_right(int speed)
+{
+    motor1->setSpeed(speed);
+    motor2->setSpeed(speed);
+    motor1->run(FORWARD);
+    motor2->run(BACKWARD);
+}
+
+void rotate_left(int speed)
+{
+    motor1->setSpeed(speed);
+    motor2->setSpeed(speed);
+    motor1->run(BACKWARD);
+    motor2->run(FORWARD);
+}
+
+void stop()
+{
+    motor1->setSpeed(0);
+    motor2->setSpeed(0);
+    motor1->run(RELEASE);
+    motor2->run(RELEASE);
+}
+
+/******************************** LINE FOLLOWING ALGORITHM ********************************/
+void line_follow(int LineSensor1,int LineSensor2)
+{
+    if ((LineSensor1 == LOW) && (LineSensor2 == LOW))
+    {
+        forwards(motorSpeed);
+    }
+    else if ((LineSensor1 == LOW) && (LineSensor2 == HIGH))
+    {
+        turn_right_forwards(motorSpeed, motorSpeed / 4);
+    }
+    else if ((LineSensor1 == HIGH) && (LineSensor2 == LOW))
+    {
+        turn_left_forwards(motorSpeed, motorSpeed / 4);
+    }
+    else if ((LineSensor1 == HIGH) && (LineSensor2 == HIGH))
+    {
+        stop();
+        delay(2000);
+        Ifdeliver = true;
+    }
+}
+
+
+/***********************delivery*******************/
+void red_box()
+{   forwards(motorSpeed/3);
+    delay(duration_delivery);
+    turn_right_forwards(motorSpeed/3);
+    delay(duration_90degree);
+    forwards(motorSpeed/3);
+    delay(duration_delivery);
+    backwards(motorSpeed/3);
+    delay(duration_delivery);
+    turn_right_forwards(motorSpeed/3);
+    delay(duration_90degree);
+    stop();
+    Ifdeliver = false;
+}
+
+void blue_box()
+{
+    forwards(motorSpeed/3);
+    delay(duration_delivery);
+    turn_left_forwards(motorSpeed/3);
+    delay(duration_90degree);
+    forwards(motorSpeed/3);
+    delay(duration_delivery);
+    backwards(motorSpeed/3);
+    delay(duration_delivery);
+    turn_left_forwards(motorSpeed/3);
+    delay(duration_90degree);
+    stop()
+    Ifdeliver = false;
+}
+

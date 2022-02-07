389cc963c4a3fd15fc02e758b7be050369d39d6d
Charlie-Brunt <cwhbrunt@gmail.com>
2022-02-07 11:20:22 +0000

    detect_block_Test

---
+++ detect_block_test.ino
@@ -0,0 +1,265 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+#define leftIn A0
+#define rightIn A1
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
+
+const int motionLEDpin = 13;
+int junctionCounter = 0;
+
+const float motorSpeed = 255; // Adjust motor speed here
+const int turningRate = 0;    // Potential for turning rate adjustment?
+const int duration = 1000;
+bool lfReverse = false; 
+bool IsOffLine = false;
+bool IfRotate = false;
+
+int LineSensor1;
+int LineSensor2;
+
+const int duration_90degree = 3500;
+const int duration_delivery = 2000;
+
+// function definitions
+void forwards();
+void stop();
+void turn_right_forwards();
+void turn_left_forwards();
+void turn_right_backwards();
+void turn_left_backwards();
+void rotate_left();
+void rotate_right();
+void updateLineSensors(int threshold = 850);
+
+void setup()
+{
+    AFMS.begin();
+    pinMode(motionLEDpin, OUTPUT);
+    pinMode(leftIn, INPUT);
+    pinMode(rightIn, INPUT);
+
+    Serial.begin(9600);
+    Serial.println("Ready!");
+    delay(3000);
+}
+
+void loop()
+{    
+    updateLineSensors(850);
+
+    if (IfRotate == true) {
+        rotate180();
+    }
+    else{
+        Serial.println("line follow");
+        line_follow();
+    }
+}
+
+void forwards(int speed)
+{
+    motor1->setSpeed(speed);
+    motor2->setSpeed(speed);
+    motor1->run(FORWARD);
+    motor2->run(FORWARD);
+}
+
+void backwards(int speed)
+{
+    motor1->setSpeed(speed);
+    motor2->setSpeed(speed);
+    motor1->run(BACKWARD);
+    motor2->run(BACKWARD);
+}
+
+void turn_right_forwards(int speed_high, int speed_low)
+{
+    motor1->setSpeed(speed_high);
+    motor2->setSpeed(speed_low);
+    motor1->run(FORWARD);
+    motor2->run(FORWARD);
+}
+
+void turn_left_forwards(int speed_high, int speed_low)
+{
+    motor1->setSpeed(speed_low);
+    motor2->setSpeed(speed_high);
+    motor1->run(FORWARD);
+    motor2->run(FORWARD);
+}
+
+void turn_left_backwards(int speed_high, int speed_low)
+{
+    motor1->setSpeed(speed_high);
+    motor2->setSpeed(speed_low);
+    motor1->run(BACKWARD);
+    motor2->run(BACKWARD);
+}
+
+void turn_right_backwards(int speed_high, int speed_low)
+{
+    motor1->setSpeed(speed_low);
+    motor2->setSpeed(speed_high);
+    motor1->run(BACKWARD);
+    motor2->run(BACKWARD);
+}
+
+void rotate_right(int speed)
+{
+    motor1->setSpeed(speed);
+    motor2->setSpeed(speed);
+    motor1->run(FORWARD);
+    motor2->run(BACKWARD);
+}
+
+void rotate_left(int speed)
+{
+    motor1->setSpeed(speed);
+    motor2->setSpeed(speed);
+    motor1->run(BACKWARD);
+    motor2->run(FORWARD);
+}
+
+void stop()
+{
+    motor1->setSpeed(0);
+    motor2->setSpeed(0);
+    motor1->run(RELEASE);
+    motor2->run(RELEASE);
+    digitalWrite(motionLEDpin, LOW);
+
+}
+/******************************** 180 TURN ********************************/
+void rotate180()
+{
+    if (IsOffLine == false)
+    {
+        rotate_right(motorSpeed);
+        delay(1000);
+        IsOffLine = true;
+    }
+    else
+    {
+        if (LineSensor2 == LOW)
+        {
+            rotate_right(motorSpeed);
+        }
+        else if (LineSensor2 == HIGH)
+        {
+            stop();
+            IsOffLine = false;
+            IfRotate=false;
+        }
+    }
+}
+/******************************** LINE FOLLOWING ALGORITHM ********************************/
+void line_follow()
+{
+    if ((LineSensor1 == LOW) && (LineSensor2 == LOW))
+    {
+        forwards(motorSpeed);
+    }
+    else if ((LineSensor1 == LOW) && (LineSensor2 == HIGH))
+    {
+        turn_right_forwards(motorSpeed, motorSpeed / 4);
+    }
+    else if ((LineSensor1 == HIGH) && (LineSensor2 == LOW))
+    {
+        turn_left_forwards(motorSpeed, motorSpeed / 4);
+    }
+    else if ((LineSensor1 == HIGH) && (LineSensor2 == HIGH))
+    {
+        switch (junctionCounter)
+        {
+        case 0:
+            forwards(motorSpeed);
+            junctionCounter++;
+            delay(1500);
+            break;
+        case 1:
+            forwards(motorSpeed);
+            junctionCounter++;
+            delay(1500);
+            break;
+        case 2:
+            stop();
+            IfRotate = true;
+            junctionCounter++;
+            break;
+        case 3:
+            stop();
+            blue_box();
+            junctionCounter = 2;
+            break;
+        }
+    }
+}
+/********************** LINE SENSOR UPDATE STATE ***************************/
+void updateLineSensors(int threshold = 850) {
+    //sets left LineSensor1 to high if on tape, else Low
+    if (analogRead(leftIn) >= threshold)
+    {
+        LineSensor1 = HIGH;
+    }
+    else
+    {
+        LineSensor1 = LOW;
+    }
+    //sets right LineSensor2 to high if on tape, else Low
+    if (analogRead(rightIn) >= threshold)
+    {
+        LineSensor2 = HIGH;
+    }
+    else
+    {
+        LineSensor2 = LOW;
+    }    
+}
+
+void red_box()
+{
+    forwards(motorSpeed / 2);
+    delay(duration_delivery);
+    rotate_right(motorSpeed/2);
+    delay(duration_90degree);
+    stop();
+    delay(2000);
+    forwards(motorSpeed / 2);
+    delay(duration_delivery);
+    backwards(motorSpeed/2);
+    delay(duration_delivery);
+    rotate_right(motorSpeed/2);
+    delay(duration_90degree*0.8);
+    updateLineSensors(850);
+    while (LineSensor2 == LOW){
+      updateLineSensors(850);
+      rotate_right(motorSpeed/2);
+    }
+    stop();
+}
+
+void blue_box()
+{
+    forwards(motorSpeed / 2);
+    delay(duration_delivery);
+    rotate_left(motorSpeed/2);
+    delay(duration_90degree);
+    stop();
+    delay(2000);
+    forwards(motorSpeed / 2);
+    delay(duration_delivery);
+    backwards(motorSpeed/2);
+    delay(duration_delivery);
+    rotate_left(motorSpeed/2);
+    delay(duration_90degree*0.8);
+    updateLineSensors(850);
+    while (LineSensor1 == LOW){
+      updateLineSensors(850);
+      rotate_left(motorSpeed/2);
+    }
+    stop();
+}
